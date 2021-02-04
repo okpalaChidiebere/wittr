b60fc983399eea8aee90d9db9cd77164f8b38f6f
Chidiebere Okpala <okpalacollins4@gmail.com>
2021-02-04 13:06:51 -0600

    Hijacking Requests - 1
    We just high jacked the whole request before they hit the HTTP cache and then network, to return some basic http content

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -2,5 +2,13 @@ self.addEventListener('fetch', function(event) {
 	// TODO: respond to all requests with an html response
 	// containing an element with class="a-winner-is-me".
 	// Ensure the Content-Type of the response is "text/html"
-  console.log(event.request);
+  //console.log(event.request);
+  event.respondWith(
+      new Response('<p class="a-winner-is-me"> Hello Winner </p>', {
+          headers: {
+              'Content-Type': 'text/html', //this is important to display html in our page
+              'foo': 'bar' //we will not use this. But you can see that it is passed to our header in the network tab 
+          }
+      })
+  )
 });
