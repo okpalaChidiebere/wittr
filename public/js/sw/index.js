4163b1f3073fc27eaee51cba595631d6764c1960
Chidiebere Okpala <okpalacollins4@gmail.com>
2021-02-04 13:01:03 -0600

    Hijacking Requests - 2
    What happens is the page will load as normal, but any fetch API request for an image, we load this gif rather than the real image
    
    HelpFul resource to help use how to get data from the request in this case the url for the api
    https://developer.mozilla.org/en-US/docs/Web/API/Request

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -1,7 +1,15 @@
 self.addEventListener('fetch', function(event) {
-  // TODO: only respond to requests with a
-  // url ending in ".jpg"
-  event.respondWith(
-    fetch('/imgs/dr-evil.gif')
-  );
+    // TODO: only respond to requests with a
+    // url ending in ".jpg"
+    try{
+      const { request } = event
+      if(request.url.endsWith('.jpg')){
+          event.respondWith( //you cannot really destruct this fuction like we did for request becuase this is not a callback but actually a function
+              fetch('/imgs/dr-evil.gif')
+          )
+      }
+    }catch(e){
+        console.log('it failed')
+    }
+    
 });
