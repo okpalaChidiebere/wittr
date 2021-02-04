9036f6aa57a73b54ca6b7dbcb94785f4909a4cb4
Chidiebere Okpala <okpalacollins4@gmail.com>
2021-02-04 13:38:31 -0600

    Hijacking
    Requests 3 Quiz
    Chaining two fetch request together(fetch request making a fetch request). We basically did anytime the user enter an endpoint to a page in the browser and it fails, we return an image

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -1,15 +1,31 @@
 self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    fetch(event.request).then(function(response) {
-      if (response.status === 404) {
-        // TODO: instead, respond with the gif at
-        // /imgs/dr-evil.gif
-        // using a network request
-        return new Response("Whoops, not found");
-      }
-      return response;
-    }).catch(function() {
-      return new Response("Uh oh, that totally failed!");
-    })
-  );
+    event.respondWith(
+      fetch(event.request).then(function(response) { //a nested fetch request with service worker
+        if (response.status === 404) {
+          // TODO: instead, respond with the gif at
+          // /imgs/dr-evil.gif
+          // using a network request
+          return fetch('/imgs/dr-evil.gif')
+          //return new Response("Whoops, not found");
+        }
+        return response;
+      }).catch(function() {
+        return new Response("Uh oh, that totally failed!");
+      })
+      //customFetch (event.request)
+    );
+    
 });
+const customFetch = async (request) => {
+    try{
+        const response = await fetch(request)
+        /*const { status } = response
+        if (status === 404) {
+              return await fetch('/imgs/dr-evil.gif')
+        }*/
+        console.log(response)
+        return response
+    }catch(e){
+        return new Response("Uh oh, that totally failed!");
+    }
+  }
