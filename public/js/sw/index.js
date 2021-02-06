45770b1c88276627a3fa152d9aab1cc7ec3938b6
Chidiebere Okpala <okpalacollins4@gmail.com>
2021-02-06 14:43:33 -0600

    Log requests

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -1,3 +1,10 @@
 self.addEventListener('fetch', function(event) {
   console.log(event.request);
+  //console.log('Hello World');
 });
+
+/*Any slight change to this file, will cause a new service worker to be created in the background and wait for the 
+existing used sw to be freed from all pages before i can take over
+
+Reloading the page while holding shift will make the new updated service worker in background take over. It is kinda
+like force load :) */
